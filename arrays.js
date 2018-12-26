ce8155b311071ee5a869f449b4fc3f76ae52ecc7
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-26 23:05:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,14 +13,23 @@ function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 
+<<<<<<< HEAD
 function destructivelyAddElementToEndOfArray(array, element ) {
+=======
+function destructivelyAddElementToEndOfArray(array, element) {
+>>>>>>> f1b2a0bb0e7679c38b7e3ae844f2db74982dd79e
   array.push(element)
   return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+<<<<<<< HEAD
   array.shift()
   return array}
+=======
+  array.shift(0)
+  array}
+>>>>>>> f1b2a0bb0e7679c38b7e3ae844f2db74982dd79e
 
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)}
@@ -29,6 +38,11 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array}
   
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> f1b2a0bb0e7679c38b7e3ae844f2db74982dd79e
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)}
 
